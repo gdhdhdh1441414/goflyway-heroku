2569f438c78569247af6238f4bb4aa7176c0b1ae
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-04-18 23:36:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,24 @@ export PATH
 ######################################################
 
 
+mkdir "/caddybin"
+cd "/caddybin"
+wget -qO "caddy.tar.gz" "https://github.com/caddyserver/caddy/releases/download/v2.6.4/caddy_2.6.4_linux_amd64.tar.gz"
+tar xvf "caddy.tar.gz"
+rm -f "caddy.tar.gz"
+chmod +x "caddy"
+cd "/root"
+mkdir "/wwwroot"
+cd "/wwwroot"
+
+
+
 wget -qO /usr/bin/goflyway https://raw.githubusercontent.com/362227/kod/master/web/data/goflyway
 chmod +x /usr/bin/goflyway
 
 cat <<-EOF > "/caddybin/Caddyfile"
-http://0.0.0.0:8100
-{
-  root /wwwroot
-  index index.html index.txt
-  timeouts none
-  proxy  localhost:8100 {
-    websocket
-    header_upstream -Origin
-  }
+slimehom61615php.herokuapp.com {
+    reverse_proxy localhost:8100
 }
 EOF
 
